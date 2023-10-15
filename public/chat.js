ff886128bdfc860bb3a143999dc157dd7a703d1c
PowerDev <140966320+PowerzinBR@users.noreply.github.com>
2023-10-15 16:23:08 -0300

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,19 @@
-// no code
+const io = require('socket.io')
+const socket = io();
+
+let username = localStorage.getItem('username');
+
+function sendMessage() {
+  const message = document.getElementById('messageInput').value;
+  if (message.trim() !== '') {
+    socket.emit('chat message', { username, message });
+    document.getElementById('messageInput').value = '';
+  }
+}
+
+socket.on('chat message', (data) => {
+  const ul = document.getElementById('messages');
+  const li = document.createElement('li');
+  li.innerHTML = `<span class="username">${data.username}:</span> ${data.message}`;
+  ul.appendChild(li);
+});
